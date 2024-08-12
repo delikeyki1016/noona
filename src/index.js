b3cc203993db41e9ef7d35eb4b3229b234e085b1
delikeyki1016 <149133856+delikeyki1016@users.noreply.github.com>
2024-08-12 22:06:19 +0900

    클래스형 함수로 변경

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import App from "./App";
-// import AppClass from "./AppClass";
+// import App from "./App";
+import AppClass from "./AppClass";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     // <React.StrictMode>
     <>
-        <App />
-        {/* <AppClass /> */}
+        {/* <App /> */}
+        <AppClass />
     </>
     // </React.StrictMode>
 );
